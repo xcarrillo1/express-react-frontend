d46b36de8bd5fd63bec54a9f0342856de9dbe67c
xcarrillo1 <Carrillo.Xavier@outlook.com>
2021-10-19 19:13:48 -0400

    another try

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,53 +1,53 @@
-import { useEffect, useState } from "react";
-import { Route, Switch } from "react-router-dom";
-import Index from "../pages/Index";
-import Show from "../pages/Show";
+import { useEffect, useState } from "react"
+import { Route, Switch } from "react-router-dom"
+import Index from "../pages/Index"
+import Show from "../pages/Show"
 
 function Main(props) {
-  const [people, setPeople] = useState(null);
+  const [ people, setPeople ] = useState(null);
 
-  const URL = "https://express-react123.herokuapp.com/people";
+  const URL = "http://localhost:4000/people/";
 
   const getPeople = async () => {
     const response = await fetch(URL);
     const data = await response.json();
     setPeople(data);
-  };
+  }
 
-  const createPeople = async (person) => {
+  const createPeople = async person => {
     // make post request to create people
     await fetch(URL, {
-      method: "post",
+      method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "Application/json",
       },
       body: JSON.stringify(person),
     });
     // update list of people
     getPeople();
-  };
+  }
 
-  const updatePeople = async (person) => {
-    // make post request to create people
-    await fetch(URL + person._id, {
-      method: "put",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(person),
-    });
-    // update list of people
-    getPeople();
-  };
-
-  const deletePeople = async (id) => {
-    // make post request to create people
+  const updatePeople = async (person, id) => {
+    // make put request to create people
     await fetch(URL + id, {
-      method: "delete",
+      method: "PUT",
+      headers: {
+        "Content-Type": "Application/json",
+      },
+      body: JSON.stringify(person),
     });
     // update list of people
     getPeople();
-  };
+  }
+
+  const deletePeople = async id => {
+    // make delete request to create people
+    await fetch(URL + id, {
+      method: "DELETE",
+    })
+    // update list of people
+    getPeople();
+  }
 
   useEffect(() => getPeople(), []);
 
@@ -59,7 +59,7 @@ function Main(props) {
         </Route>
         <Route
           path="/people/:id"
-          render={(rp) => (
+          render={rp => (
             <Show
               people={people}
               updatePeople={updatePeople}
