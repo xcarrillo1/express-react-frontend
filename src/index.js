05e3b262aefee435095dfd6036030a836e7d3c15
xcarrillo1 <Carrillo.Xavier@outlook.com>
2021-10-19 21:07:42 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // IMPORT SCSS FILE TO BE SOURCE OF STYLING
-import './index.css';
+import './styles.scss';
 // IMPORT ROUTER
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
